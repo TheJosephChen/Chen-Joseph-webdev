89d42cb110d66535a2e5b4c9a0700042398f0bf2
TheJosephChen <violuminescence@gmail.com>
2017-07-24 17:38:47 -0400

    move views into folder

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -6,31 +6,32 @@
     function configuration($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "user/templates/login.view.client.html",
+                templateUrl: "views/user/templates/login.view.client.html",
                 controller: "loginController",
                 controllerAs: "model"})
             .when("/register", {
-                templateUrl: "user/templates/register.view.client.html",
+                templateUrl: "views/user/templates/register.view.client.html",
                 controller: "registerController",
                 controllerAs: "model"})
             .when("/profile/:userId", {
-                templateUrl: "user/templates/profile.view.client.html",
+                templateUrl: "views/user/templates/profile.view.client.html",
                 controller: "profileController",
                 controllerAs: "model"})
             // website routes
             .when("/user/:userId/website", {
-                templateUrl: "website/templates/website-list.view.client.html",
+                templateUrl: "views/website/templates/website-list.view.client.html",
                 controller: "websiteListController",
                 controllerAs: "model"})
             .when("/user/:userId/website/new", {
-                templateUrl: "website/templates/website-new.view.client.html",
+                templateUrl: "views/website/templates/website-new.view.client.html",
                 controller: "newWebsiteController",
                 controllerAs: "model"})
             .when("/user/:userId/website/:websiteId/edit", {
-                templateUrl: "website/templates/website-edit.view.client.html",
+                templateUrl: "views/website/templates/website-edit.view.client.html",
                 controller: "editWebsiteController",
                 controllerAs: "model"})
-            .when("/user/:userId/page", {
-                templateUrl: "page/templates/page-list.view.client.html"})
+            // page routes
+            .when("/user/:userId/website/:websiteId/page", {
+                templateUrl: "views/page/templates/page-list.view.client.html"})
     }
 })();
--- public/assignment/views/user/controllers/profile.controller.client.js
+++ public/assignment/views/user/controllers/profile.controller.client.js
@@ -12,7 +12,6 @@
 
         function init() {
             model.user = userService.findUserByID(userId);
-
         };
         init();
 
