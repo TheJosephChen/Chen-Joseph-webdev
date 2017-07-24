b808943b0864754a9f25136b16347bf833598edf
TheJosephChen <violuminescence@gmail.com>
2017-07-24 16:01:47 -0400

    widget service functions

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -5,5 +5,73 @@
 
     function widgetService() {
 
+        var widgets = [
+            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                "url": "http://lorempixel.com/400/200/"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        ];
+
+        this.createWidget = createWidget;
+        this.findWidgetsByPageId = findWidgetsByPageId;
+        this.findWidgetById = findWidgetById;
+        this.updateWidget = updateWidget;
+        this.deleteWidget = deleteWidget;
+
+        function createWidget(pageId, widget) {
+            widget._id = (new Date()).getTime() + "";
+            widget.pageId = widgetId;
+            widgets.push(widget);
+            return widget;
+        }
+
+        function findWidgetsByPageId(pageId) {
+            var widgets = [];
+            for (var w in widgets) {
+                if (widgets[w].pageId === pageId) {
+                    widgets.push(widgets[w]);
+                }
+            }
+            return widgets;
+        }
+
+        function findWidgetById(widgetId) {
+            for (var w in widgets) {
+                var widget = widgets[w];
+                if (widget._id === widgetId) {
+                    return widget;
+                }
+            }
+            return null;
+        }
+
+        function updateWidget(widgetId, widget) {
+            for (var w in widgets) {
+                if (widgets[w]._id === widgetId) {
+                    widgets[w] = widget;
+                    return widget;
+                }
+            }
+            return null;
+        }
+
+        function deleteWidget(widgetId) {
+            var widgetIndex;
+            for (var w in widgets) {
+                if (widgets[w]._id === widgetId) {
+                    widgetIndex = p;
+                    widgets.splice(widgetIndex, 1);
+                    return widgetId;
+                }
+            }
+            return null;
+        }
+
+
     }
 })();
