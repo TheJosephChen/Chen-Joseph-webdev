9ed406e275b38ad7bceb91d578ceced414d593e0
TheJosephChen <violuminescence@gmail.com>
2017-07-24 15:41:55 -0400

    website service functions

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -15,9 +15,21 @@
             { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
         ];
 
-        this.findWebsitesForUser = findWebsitesForUser;
+        this.createWebsite = createWebsite;
+        this.findWebsitesByUser = findWebsitesByUser;
+        this.findWebsiteById = findWebsiteById;
+        this.updateWebsite = updateWebsite;
+        this.deleteWebsite = deleteWebsite;
 
-        function findWebsitesForUser(userId) {
+        function createWebsite(userId, website) {
+            website._id = (new Date()).getTime() + "";
+            website.developerId = userId;
+            websites.push(website);
+            return website;
+
+        }
+
+        function findWebsitesByUser(userId) {
             var sites = [];
             for (var w in websites) {
                 if (websites[w].developerId === userId) {
@@ -25,6 +37,38 @@
                 }
             }
             return sites;
+        }
+
+        function findWebsiteById(websiteId) {
+            for (var w in websites) {
+                var website = websites[w];
+                if (website._id === websiteId) {
+                    return website;
+                }
+            }
+            return null;
+        }
+
+        function updateWebsite(websiteId, website) {
+            for (var w in websites) {
+                if (websites[w]._id === websiteId) {
+                    websites[w] = website;
+                    return website;
+                }
+            }
+            return null;
+        }
+
+        function deleteWebsite(websiteId) {
+            var websiteIndex;
+            for (var w in websites) {
+                if (websites[w]._id === websiteId) {
+                    websiteIndex = w;
+                    websites.splice(websiteIndex, 1);
+                    return websiteId;
+                }
+            }
+            return null;
         }
     }
 })();
