638f625a8aec12223b47c01495c652a455ebdb48
TheJosephChen <violuminescence@gmail.com>
2017-07-24 20:05:41 -0400

    finish widget display

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -3,16 +3,30 @@
         .module("WamApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController($routeParams, widgetService) {
+    function widgetListController($routeParams, widgetService, $sce) {
         var model = this;
 
         model.userId = $routeParams.userId;
         model.websiteId = $routeParams.websiteId;
         model.pageId = $routeParams.pageId;
+        model.trustThisHTMLContent = trustThisHTMLContent;
+        model.getYouTubeEmbedUrl = getYouTubeEmbedUrl;
         function init() {
             model.widgets = widgetService.findWidgetsByPageId(model.pageId);
         }
         init();
+
+        function trustThisHTMLContent(html) {
+            return $sce.trustAsHtml(html);
+        }
+
+        function getYouTubeEmbedUrl(youtubeLink) {
+            var embedUrl = 'https://www.youtube.com/embed/';
+            var linkParts = youtubeLink.split('/');
+            var id = linkParts[linkParts.length - 1];
+            embedUrl += id;
+            return $sce.trustAsResourceUrl(embedUrl);
+        }
 
     }
 })();
