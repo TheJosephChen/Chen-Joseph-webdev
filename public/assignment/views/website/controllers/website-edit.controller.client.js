303341b78c6d8fa4b1a5a5cf8b88c3f54a4f3796
TheJosephChen <violuminescence@gmail.com>
2017-07-25 12:12:26 -0400

    Fix website edit

--- public/assignment/views/website/controllers/website-edit.controller.client.js
+++ public/assignment/views/website/controllers/website-edit.controller.client.js
@@ -10,20 +10,31 @@
         model.websiteId = $routeParams.websiteId;
         model.updateWebsite = updateWebsite;
         model.deleteWebsite = deleteWebsite;
+        model.cancelEdit = cancelEdit;
 
         function init() {
-            model.websites = websiteService.findWebsitesByUser(model.userId);
-            model.website = websiteService.findWebsiteById(model.websiteId);
+            model.websites = angular.copy(websiteService.findWebsitesByUser(model.userId));
+            model.website = angular.copy(websiteService.findWebsiteById(model.websiteId));
+            model.origWebsite = angular.copy(model.website);
+            model.origWebsites = angular.copy(model.websites)
         }
         init();
 
         function updateWebsite(website) {
             websiteService.updateWebsite(website._id, website);
+            angular.copy(model.website, model.origWebsite);
+            angular.copy(model.websites, model.origWebsites);
             $location.url("/user/" + model.userId + "/website");
         }
 
         function deleteWebsite(websiteId) {
             websiteService.deleteWebsite(websiteId);
+            $location.url("/user/" + model.userId + "/website");
+        }
+
+        function cancelEdit() {
+            model.website = angular.copy(model.origWebsite);
+            model.websites = angular.copy(model.origWebsites);
             $location.url("/user/" + model.userId + "/website");
         }
     }
