6e2b0806a0d3d2656b52b5fdb82db457264f6f0a
TheJosephChen <violuminescence@gmail.com>
2017-08-09 22:56:37 -0400

    flickr support

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -65,6 +65,10 @@
                 templateUrl: "views/widget/templates/widget-edit.view.client.html",
                 controller: "editWidgetController",
                 controllerAs: "model"})
+            .when("/user/:userId/website/:websiteId/page/:pageId/widget/:widgetId/search", {
+                templateUrl: "views/widget/templates/widget-flickr-search.view.client.html",
+                controller: "flickrController",
+                controllerAs: "model"})
 
 
     }
