cee2932d46e5ba3abd3653ea894140bdf366135e
TheJosephChen <violuminescence@gmail.com>
2017-08-17 17:35:43 -0400

    admin delete tournaments

--- public/project/views/tournament/controllers/tournament-list.controller.js
+++ public/project/views/tournament/controllers/tournament-list.controller.js
@@ -6,7 +6,8 @@
     function tournamentListController(tournamentService, getLoggedInUser) {
         var model = this;
         model.tournaments = [];
-        model.loggedInUser = getLoggedInUser
+        model.loggedInUser = getLoggedInUser;
+        model.deleteTournament = deleteTournament;
 
         function init() {
             tournamentService
@@ -16,6 +17,15 @@
                 })
         }
         init();
+
+        // for admins only
+        function deleteTournament(tournamentId) {
+            tournamentService
+                .deleteTournament(tournamentId)
+                .then(function () {
+                    init();
+                })
+        }
 
     }
 
